f5701dca473c4d70f6e40cf0e39e50b2bbbcaac4
ILYASSE1 <96141502+ILYASSE1@users.noreply.github.com>
2022-02-03 15:46:59 +0100

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,80 @@
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+clear
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+sleep 0.4
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+sleep 0.4
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo "
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+    |B|y|I|L|Y|A|S|S|E| |i|n| |T|e|r|m|u|x|
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+            +-+-+ +-+-+-+-+-+-+-+-+-+-+
+            |b|y| |G|u|s|h|m|a|z|u|k|o|
+            +-+-+ +-+-+-+-+-+-+-+-+-+-+
+"
+echo "+++..........|I|L|Y|A|S|S|E|+++............"
+apt install bash && apt install fish && apt install python && apt install python2 && apt install python3 && apt install nano && apt install php && apt install nmap && git clone https://github.com/rajkumardusad/IP-Tracer && ls && echo "++++++++DONE+++++++"+
+echo "ILYASSE"
+apt upgrade && apt install cmatrix && cmatrix
+echo "Ctrl+q=exit on cmatrix"
+toilet By=script.ilyasse
